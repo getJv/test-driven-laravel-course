3c0e04667d51e2a4da1cfd0ad6a0daae9d7a4f6b
Jhonatan Vinicius Paulino de Morais <jhonatan.morais@epl.gov.br>
2019-09-30 18:41:13 -0300

    final class cap 4

--- app/Book.php
+++ app/Book.php
@@ -1,21 +1,56 @@
 <?php
 
 namespace App;
+
 use App\Author;
+use App\Reservation;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    public $guarded =[];
+    public $guarded = [];
 
-    public function path(){
-        return '/books/' . $this->id . '-' . \Str::slug($this->title); 
+    public function path()
+    {
+        return '/books/' . $this->id . '-' . \Str::slug($this->title);
     }
 
 
-    public function setAuthorIdAttribute($author){
+    public function setAuthorIdAttribute($author)
+    {
         $this->attributes['author_id'] = (Author::firstOrCreate([
             'name' => $author,
         ]))->id;
+    }
+
+    public function checkout($user)
+    {
+
+        $this->reservations()->create([
+            'user_id'           => $user->id,
+            'checked_out_at'    => now(),
+        ]);
+    }
+
+    public function checkin($user)
+    {
+        $reservation = $this->reservations()->where('user_id', $user->id)
+            ->whereNotNull('checked_out_at')
+            ->whereNull('checked_in_at')
+            ->first();
+
+        if(is_null($reservation)){
+            throw new \Exception();
+        }
+
+        $reservation->update([
+            'checked_in_at' => now(),
+        ]);
+    }
+
+    public function reservations()
+    {
+
+        return $this->hasMany(Reservation::class);
     }
 }
